9e32c5ac1c0bb40a56bad758dbfa2da501ed9f19
Daniel Giao <daniel.giao@icloud.com>
2024-09-03 13:52:15 -0400

    SwiftUI setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # swiftui-tinder
-# swiftui-tinder
--- SwiftUI-tinder/App/SwiftUI_tinderApp.swift
+++ SwiftUI-tinder/App/SwiftUI_tinderApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct SwiftUI_tinderApp: App {
+struct TinderApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
