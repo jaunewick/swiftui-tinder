5a8e76d9f49188341bf742dc94d550e741bc93ca
Daniel Giao <daniel.giao@icloud.com>
2024-09-03 14:27:29 -0400

    Added static card

---
+++ SwiftUI-tinder/Core/Cards/CardView.swift
@@ -0,0 +1,33 @@
+//
+//  CardView.swift
+//  SwiftUI-tinder
+//
+//  Created by Daniel Giao on 03/09/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Image(.meganFox1)
+                .resizable()
+                .scaledToFill()
+                .frame(width: cardWidth, height: cardHeight)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+}
+
+private extension CardView {
+    var cardWidth: CGFloat {
+        UIScreen.main.bounds.width - 20
+    }
+    var cardHeight: CGFloat {
+        UIScreen.main.bounds.height / 1.45
+    }
+}
+
+#Preview {
+    CardView()
+}
